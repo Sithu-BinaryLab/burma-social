05d52213c3a481e0ac44d3f4919a3737547e2462
Sithu-BinaryLab <sithulwin.binarylab@gmail.com>
2023-10-09 11:12:34 +0630

    setup: project setup

---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <div className="h-screen flex justify-center items-center bg-slate-200">
+      page
+    </div>
+  );
+};
+
+export default SignUp;
